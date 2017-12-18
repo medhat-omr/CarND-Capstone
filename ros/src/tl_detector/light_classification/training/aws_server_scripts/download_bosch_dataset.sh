9c8b61e102a42737610d983c1b951514b4d6d42d
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2017-12-18 22:03:45 +0300

    Fix work of scripts

--- ros/src/tl_detector/light_classification/training/aws_server_scripts/download_bosch_dataset.sh
+++ ros/src/tl_detector/light_classification/training/aws_server_scripts/download_bosch_dataset.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
-BOSCH_DATASET_NAME=bosch_dataset.zip
+BOSCH_DATASET_NAME=bosch-dataset.zip
 BOSCH_DATASET_ID=195tf4MTzzZpO1Sft5RLY5E4B6OgkmykN
 GDRIVE_DOWNLOAD_URL=https://drive.google.com/uc?export=download
 
+[ -e ${BOSCH_DATASET_NAME} ] && rm ${BOSCH_DATASET_NAME}
 curl -sc /tmp/gcokie "${GDRIVE_DOWNLOAD_URL}&id=${BOSCH_DATASET_ID}" >/dev/null  
 CONFIRM_ID="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
-curl -LOJb /tmp/gcokie "${GDRIVE_DOWNLOAD_URL}&confirm=${CONFIRM_ID}&id=${BOSCH_DATASET_ID}" -o ${BOSCH_DATASET_NAME}
+curl -LOJb /tmp/gcokie "${GDRIVE_DOWNLOAD_URL}&confirm=${CONFIRM_ID}&id=${BOSCH_DATASET_ID}"
 unzip ${BOSCH_DATASET_NAME}
