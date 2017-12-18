e1fa4fafea4220fa873617012c5af141885fcfab
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2017-12-18 21:37:44 +0300

    Fix a bug in download_bosch_dataset.sh

--- ros/src/tl_detector/light_classification/training/aws_server_scripts/download_bosch_dataset.sh
+++ ros/src/tl_detector/light_classification/training/aws_server_scripts/download_bosch_dataset.sh
@@ -3,7 +3,7 @@ BOSCH_DATASET_NAME=bosch_dataset.zip
 BOSCH_DATASET_ID=195tf4MTzzZpO1Sft5RLY5E4B6OgkmykN
 GDRIVE_DOWNLOAD_URL=https://drive.google.com/uc?export=download
 
-curl -sc /tmp/gcokie "${GDRIVE_DOWNLOAD_URL}&id=${ggID}" >/dev/null  
+curl -sc /tmp/gcokie "${GDRIVE_DOWNLOAD_URL}&id=${BOSCH_DATASET_ID}" >/dev/null  
 CONFIRM_ID="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
 curl -LOJb /tmp/gcokie "${GDRIVE_DOWNLOAD_URL}&confirm=${CONFIRM_ID}&id=${BOSCH_DATASET_ID}" -o ${BOSCH_DATASET_NAME}
 unzip ${BOSCH_DATASET_NAME}
