0684941f3fc161bb66c5797c1503733072d1b760
ibrahim-mo <heema111@gmail.com>
2017-12-19 03:40:42 -0800

    A few improvements for PID controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -3,40 +3,44 @@ from pid import PID
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-CMD_RATE = 50
+
+CMD_RATE = 10 # 10Hz
+MIN_SPEED = 0.0
+KP = 1.0
+KI = 0.0001
+KD = 10.0
+MAX_ACCEL = 5.0
 
 class Controller(object):
-    def __init__(self, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
-        self._wheel_base = wheel_base
-        self._steer_ratio = steer_ratio
-        self._max_lat_accel = max_lat_accel
-        self._max_steer_angle = max_steer_angle
+    def __init__(self, wheel_base, steer_ratio, max_lat_accel, max_steer_angle,
+                 vehicle_mass, wheel_radius, brake_deadband):
 
-    def control(self, trgt_lin_vel, trgt_ang_vel, curr_lin_vel, dbw_enabled):
-        yaw_controller = YawController(self._wheel_base, self._steer_ratio, ONE_MPH,
-                                       self._max_lat_accel, self._max_steer_angle)
-        steer = yaw_controller.get_steering(trgt_lin_vel, trgt_ang_vel, curr_lin_vel)
+        self.yaw_controller = YawController(wheel_base, steer_ratio, MIN_SPEED, max_lat_accel, max_steer_angle)
+        self.pid = PID(KP, KI, KD, -MAX_ACCEL, MAX_ACCEL)
+        self._vehicle_mass = vehicle_mass
+        self._wheel_radius = wheel_radius
+        self._brake_deadband = brake_deadband
 
-        pid = PID(kp=1.0, ki=0.5, kd=0.0, mn=-5.0, mx=5.0)
-        err = trgt_lin_vel - curr_lin_vel
-        dt = 1.0 / CMD_RATE
-        acc = pid.step(err, dt)
-
-        # If dbw was disabled
+    def control(self, linear_velocity, angular_velocity, current_velocity, dbw_enabled):
         if not dbw_enabled:
-            pid.reset()
+            self.pid.reset()
             return 0., 0., 0.
 
-        ### May need to add a low pass filter to acc if jerk becomes and issue.
+        steer = self.yaw_controller.get_steering(linear_velocity, angular_velocity, current_velocity)
+
+        error = linear_velocity - current_velocity
+        dt = 1.0 / CMD_RATE
+        accel = self.pid.step(error, dt)
+
+        ### May need to add a low pass filter to accel if jerk becomes an issue.
 
         throttle = 0.0
         brake = 0.0
 
-        if acc > 0.0:
-            throttle = acc / 5.0
+        if accel > 0.0:
+            throttle = accel / MAX_ACCEL
 
-        if acc <= 0.0:
-            # brake = acc * '~vehicle_mass' * '~wheel_radius' 1736.35*.2413
-            brake = -1.0 * acc * 1736.35 * .2413
+        if accel < -self._brake_deadband:
+            brake = -accel * self._vehicle_mass * self._wheel_radius
 
         return throttle, brake, steer
