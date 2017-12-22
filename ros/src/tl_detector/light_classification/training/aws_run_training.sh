6113769998880d15bc8daeee38c484472409df2e
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2017-12-22 23:21:10 +0300

    Fix a bug in aws_run_training.sh

--- ros/src/tl_detector/light_classification/training/aws_run_training.sh
+++ ros/src/tl_detector/light_classification/training/aws_run_training.sh
@@ -9,7 +9,7 @@ function run_training {
     GIT_BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
     AWS_PARAMS="-i "$1" ubuntu@$2"
     ssh ${AWS_PARAMS} "git clone ${GITHUB_URL}"
-    ssh ${AWS_PARAMS} "git checkout -b ${GIT_BRANCH} origin/${GIT_BRANCH}"
+    ssh ${AWS_PARAMS} "cd CarND-Capstone && git checkout -b ${GIT_BRANCH} origin/${GIT_BRANCH}"
     ssh ${AWS_PARAMS} 'bash -s' < ./aws_server_scripts/to_be_run_on_aws.sh
     sftp ${AWS_PARAMS}/CarND-Capstone/ros/src/tl_detector/light_classification/training .
 }
