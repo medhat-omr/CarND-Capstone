4c90e2a94a1fda609e39c6b0ab178e043869fc49
bjmeier <bjmeier@users.noreply.github.com>
2017-12-28 20:58:15 -0500

    Update waypoint_updater.py
    Changed CMD_RATE to 50
    Changed LOOKAHEAD_WPS to 50

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,8 +23,8 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-CMD_RATE = 10 # 10Hz
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
+CMD_RATE = 50 # 50Hz
 ONE_KPH = 1000.0 / 3600
 # velocity in simulator is 40 k/h
 MAX_VELOCITY = 40 * ONE_KPH
